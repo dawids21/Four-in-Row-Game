dbcdc0cf13e42bfd4e3d753df89dd1c13f548d1e
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-03-17 18:44:47 +0100

    Add Cell object

---
+++ src/main/java/FourInRow/Cell.java
@@ -0,0 +1,19 @@
+package FourInRow;
+
+class Cell {
+    private int value = 0;
+    
+    Cell() {
+
+    }
+
+    int getValue() {
+        return value;
+    }
+
+    void setValue(int value) {
+        this.value = value;
+    }
+
+    
+}
